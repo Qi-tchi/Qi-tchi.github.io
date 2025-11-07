fdf8cd880cf6303528ae3cc02a29d2df28c607da
qi <qi.qiu@univ-lyon1.fr>
2025-11-07 15:32:42 +0100

    ...

---
+++ highlight.js
@@ -0,0 +1,9 @@
+  // Highlight active nav link based on current filename
+    (function(){
+      const links = document.querySelectorAll('.navig_link');
+      const currentFile = location.pathname.split('/').pop() || 'index.html';
+      links.forEach(l => {
+        const href = l.getAttribute('href');
+        if (href === currentFile) l.classList.add('active');
+      });
+    })();
